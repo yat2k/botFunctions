8aa626dd18a86f5c23e6ded203f89cac804b5af3
Yat <63180414+yat2k@users.noreply.github.com>
2021-03-02 23:05:53 +0530

    Update Unban.py

--- Unban.py
+++ Unban.py
@@ -4,3 +4,10 @@
 async def unban(ctx, *, member):
     banned_users = await ctx.guild.bans()
     member_name, member_discriminator = member.split("#")
+    
+    for ban_entry in banned_users:               #checks the banned user list and then compares with the argument and then unbans the player that matches :P
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}'
