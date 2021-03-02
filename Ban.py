9fa26423cdaf8e220c290deed877881db3771241
Yat <63180414+yat2k@users.noreply.github.com>
2021-03-02 23:03:12 +0530

    Update Ban.py

--- Ban.py
+++ Ban.py
@@ -1,6 +1,6 @@
 #The below code bans player
 @client.command()
-@commands.has_permissions(ban_members = True)
+@commands.has_permissions(ban_members = True)        #make sure to not give it to everyone xD hehe
 async def ban(ctx, member : discord.Member, *, reason = None):
     await member.ban(reason = reason)
 
